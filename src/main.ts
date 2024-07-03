4a67642f5326696e2280c01e77393fba31106fac
zukxu <zukxu520@163.com>
2024-07-03 14:27:01 +0800

    feat: 新增基础配置

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,13 @@
-import { createApp } from 'vue'
-import './style.css'
+import {createApp} from 'vue'
+import PrimeVue from 'primevue/config';
+import Aura from '@primevue/themes/aura';
+// 引入uno.css
+import 'virtual:uno.css'
+import store from '@/store/index'
+import router from '@/router/index'
+
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App);
+app.use(store).use(router).use(PrimeVue, {ripple: true, theme: Aura})
+app.mount('#app')
