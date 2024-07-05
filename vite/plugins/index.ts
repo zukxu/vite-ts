9e55e30ebc1e085c4370a830d6619745a961af50
zukxu <zukxu520@163.com>
2024-07-05 14:57:16 +0800

    [fix]: 优化自动导入

--- vite/plugins/index.ts
+++ vite/plugins/index.ts
@@ -1,11 +1,12 @@
 import vue from '@vitejs/plugin-vue';
 import Component from 'unplugin-vue-components/vite';
 import {PrimeVueResolver} from '@primevue/auto-import-resolver';
+import UnoCss from 'unocss/vite'
 import createAutoImport from "./auto-import";
 import createSvgIcons from "./svg-icon";
 
 export function createVitePlugins(env, isBuild = false) {
-  const vitePlugins = [vue(), Component({resolvers: [PrimeVueResolver()]})]
+  const vitePlugins = [vue(), Component({resolvers: [PrimeVueResolver()]}), UnoCss({})]
   vitePlugins.push(createAutoImport())
   vitePlugins.push(createSvgIcons(isBuild))
   return vitePlugins
